7551f97c98133a8a2f17e56a583fdef916a94cdf
Амир <bluesun@gmail.com>
2026-06-07 05:48:38 +0000

    terraform: addd output block to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,14 @@ resource "local_file" "example" {
   content = var.content
   filename = "${path.module}/${var.filename}"
 }
+
+output "file_path" {
+  description = "Path to the created file"
+  value       = local_file.example.filename
+}
+
+output "file_content"{
+  description = "Content of the created file"
+  value       = local_file.example.content
+  sensitive   = true
+}
